6a850153ebf9d9acfc7b3777b056dc7291dd0bae
andrew <andrewjthomsen@yahoo.com>
2019-07-22 22:29:04 -0700

    Began working with express on a project

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express()
+
+app.get('', (req, res) => {
+    res.send('Hello express')
+})
+
+app.get('/help', (req, res) => {
+    res.send('Help page')
+})
+app.get('/about', (req, res) => {
+    res.send('Title page')
+})
+app.get('/weather', (req, res) => {
+    res.send('Weather page')
+})
+app.listen(3000, () => {
+    console.log('Server running on PORT 3000.')
+})
