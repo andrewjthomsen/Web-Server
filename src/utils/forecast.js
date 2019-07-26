79a3fa1a97d6646c35d769298abd79969ce1618b
andrew <andrewjthomsen@yahoo.com>
2019-07-25 22:45:34 -0700

    Made changes to forecast.js file

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,27 +1,32 @@
 const request = require("request");
 
-
-
-
 const forecast = (latitude, longitude, callback) => {
   const url =
-  "https://api.darksky.net/forecast/33e35c6c697abb35e82b242fae88e2f8/" + latitude + "," + longitude;
-    request({url, json: true }, (error, { body }) => {
-        if(error) {
-            callback("Unable to connect to weather service!", undefined)
-        } else if (body.error) {
-            callback("Unable to find location. Try searching again... ", undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary +
-                        " It is currently " +
-                        body.currently.temperature +
-                        " degrees out. There is a " +
-                        body.currently.precipProbability +
-                        "% chance of rain.")
-        }
-    })
-
+    "https://api.darksky.net/forecast/33e35c6c697abb35e82b242fae88e2f8/" +
+    latitude +
+    "," +
+    longitude;
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location. Try searching again... ", undefined);
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          " It is currently " +
+          body.currently.temperature +
+          " degress out. This high today is " +
+          body.daily.data[0].temperatureHigh +
+          " with a low of " +
+          body.daily.data[0].temperatureLow +
+          ". There is a " +
+          body.currently.precipProbability +
+          "% chance of rain."
+      );
+    }
+  });
 };
 
-
-module.exports = forecast
+module.exports = forecast;
